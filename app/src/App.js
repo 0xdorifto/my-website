fdb0151b86a3a657f43936dba022a9304920a75d
0xdorifto <gastaosousafaria@tecnico.ulisboa.pt>
2022-12-28 19:24:36 +0000

    Add router and pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import Art from "./pages/Art";
+import Vehicles from "./pages/Vehicles";
+import Music from "./pages/Music";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/art" element={<Art />} />
+        <Route path="/vehicles" element={<Vehicles />} />
+        <Route path="/music" element={<Music />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ app/src/components/Header.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="flex flex-row bg-slate-700">
+      <div className="header-button">
+        <Link to="/">About me</Link>
+      </div>
+      <div className="header-button">
+        <Link to="/art">Art</Link>
+      </div>
+      <div className="header-button">
+        <Link to="/vehicles">Cars/Bikes</Link>
+      </div>
+      <div className="header-button">
+        <Link to="/music">Music</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
