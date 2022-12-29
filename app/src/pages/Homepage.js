b1e97c1a9e07377ee7921eff4d1f2555ace5db5f
0xdorifto <gastaosousafaria@tecnico.ulisboa.pt>
2022-12-29 23:02:27 +0000

    Add bio

--- app/src/pages/Homepage.js
+++ app/src/pages/Homepage.js
@@ -1,5 +1,15 @@
 const Homepage = () => {
-  return <p>Homepage</p>;
+  return (
+    <>
+      <p>Homepage</p>
+      <p>
+        Hi, my name is Gastão, I'm 23 and I'm a Software Engineer. I got My BSc
+        of Computer Science and Engineering at IST, University of Lisbon in
+        2022. As of now, I'm working as a React Front-End Developer. In this
+        website you can get to know about me!
+      </p>
+    </>
+  );
 };
 
 export default Homepage;
